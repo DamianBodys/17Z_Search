1a939168b4adcdb7cdd15da2e61c3a9d4743e5b6
dbodys.94 <dbodys.94@gmail.com>
2017-10-22 22:03:00 +0200

    added tool deploytoGAEstd.bat to export both 17Z_search and 17Z project

--- tools/deploytoGAEstd.bat
+++ tools/deploytoGAEstd.bat
@@ -2,4 +2,7 @@ cd
 pushd .
 call "C:\Program Files (x86)\Google\Cloud SDK\cloud_env.bat"
 popd
+cd ..\17Z_search
 gcloud app deploy app.yaml --project zsearch1 -v 2
+cd ..\17Z
+gcloud app deploy --project zflexible1 -v 1
