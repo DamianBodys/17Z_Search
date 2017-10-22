7534477683d092c013d3645c7a38c265d28d4ac0
dbodys.94 <dbodys.94@gmail.com>
2017-10-22 19:26:06 +0200

    converted indentation in swagger.py

--- swagger.py
+++ swagger.py
@@ -1,9 +1,11 @@
 import webapp2
 import json
 
-class swaggerHandler(webapp2.RequestHandler):
+
+class SwaggerHandler(webapp2.RequestHandler):
+
     def get(self):
-		data=json.load(open('static/swagger.json'))
+    	data=json.load(open('static/swagger.json'))
 		self.response.headers.add_header("Access-Control-Allow-Origin", "*")
 		self.response.headers.add_header('Access-Control-Allow-Methods', 'POST, GET, DELETE, PUT, OPTIONS')
 		self.response.headers.add_header('Access-Control-Allow-Headers', 'Content-Type, api_key, Authorization, x-requested-with, Total-Count, Total-Pages, Error-Message')
@@ -11,5 +13,5 @@ class swaggerHandler(webapp2.RequestHandler):
 		json.dump(data,self.response.out)
 		
 app = webapp2.WSGIApplication([
-    ('/swagger.json', swaggerHandler)
+    ('/swagger.json', SwaggerHandler)
 ], debug=True)
