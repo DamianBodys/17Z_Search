c34f40c6e7697cec7515478749881301f42c99f0
dbodys94 <dbodys.94@gmail.com>
2017-10-22 19:36:14 +0200

    further changes in swagger.py indentation

--- swagger.py
+++ swagger.py
@@ -5,13 +5,13 @@ import json
 class SwaggerHandler(webapp2.RequestHandler):
 
     def get(self):
-    	data=json.load(open('static/swagger.json'))
-		self.response.headers.add_header("Access-Control-Allow-Origin", "*")
-		self.response.headers.add_header('Access-Control-Allow-Methods', 'POST, GET, DELETE, PUT, OPTIONS')
-		self.response.headers.add_header('Access-Control-Allow-Headers', 'Content-Type, api_key, Authorization, x-requested-with, Total-Count, Total-Pages, Error-Message')
-		self.response.headers['Content-Type'] = 'application/json'
-		json.dump(data,self.response.out)
-		
+        data = json.load(open('static/swagger.json'))
+        self.response.headers.add_header("Access-Control-Allow-Origin", "*")
+        self.response.headers.add_header('Access-Control-Allow-Methods', 'POST, GET, DELETE, PUT, OPTIONS')
+        self.response.headers.add_header('Access-Control-Allow-Headers', 'Content-Type, api_key, Authorization, x-requested-with, Total-Count, Total-Pages, Error-Message')
+        self.response.headers['Content-Type'] = 'application/json'
+        json.dump(data, self.response.out)
+
 app = webapp2.WSGIApplication([
     ('/swagger.json', SwaggerHandler)
 ], debug=True)
