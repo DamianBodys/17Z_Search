e698c31c69bc6b3da9457b495ad8ff9e1c9b66b8
dbodys94 <dbodys.94@gmail.com>
2017-11-01 17:38:11 +0100

    deploytoGAEstd.bat changed to linux/windows

--- tools/deploytoGAEstd.bat
+++ tools/deploytoGAEstd.bat
@@ -2,7 +2,9 @@ cd
 pushd .
 call "C:\Program Files (x86)\Google\Cloud SDK\cloud_env.bat"
 popd
-cd ..\17Z_search
+cd ..
+cd 17Z_search
 gcloud app deploy app.yaml --project zsearch1 -v 2
-cd ..\17Z
+cd ..
+cd 17Z
 gcloud app deploy --project zflexible1 -v 1
