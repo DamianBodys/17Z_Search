0a6f8640e42632ccc843bbac9c0808c9d51d04d1
dbodys94 <dbodys.94@gmail.com>
2017-12-17 22:36:30 +0100

    first unit test

--- test_search_algorithm.py
+++ test_search_algorithm.py
@@ -4,6 +4,7 @@ pip install PyYaml"""
 import unittest
 import webtest
 import search_algorithm
+import json
 from google.appengine.ext import testbed
 from google.appengine.api import search
 
@@ -26,15 +27,25 @@ class SearchTestCase(unittest.TestCase):
 
     def testOneAlgorithm_AlgorithmsHandler(self):
         self.testbed.init_search_stub()
-        document = search_algorithm.create_document('algorithmId1',
-                                                    'algorithmSummary1',
-                                                    'displayName1',
-                                                    'linkURL1')
+        data={}
+        data['algorithmId']='algorithmId1'
+        data['algorithmSummary']='algorithmSummary1'
+        data['displayName']='displayName1'
+        data['linkURL']='linkURL1'
+        datalist=[]
+        datalist.append(data)
+        document = search_algorithm.create_document(datalist[0]['algorithmId'],
+                                                    datalist[0]['algorithmSummary'],
+                                                    datalist[0]['displayName'],
+                                                    datalist[0]['linkURL'])
         search.Index(name=search_algorithm._INDEX_STRING).put(document)
         response = self.testapp.get('/')
         self.assertEqual(200, response.status_int)
-        self.assertEqual('[{"algorithmSummary": "algorithmSummary1", "algorithmId": "algorithmId1", "displayName": "displayName1", "linkURL": "linkURL1"}]',
-                            response.normal_body)
+        datalist[0]['linkURL'] = u'changedLinkURL'
+        self.assertListEqual(datalist,json.loads(response.normal_body))
+
+        #self.assertEqual('[{"algorithmSummary": "algorithmSummary1", "algorithmId": "algorithmId1", "displayName": "displayName1", "linkURL": "linkURL1"}]',
+        #                   response.normal_body)
         self.assertEqual('application/json', response.content_type)
 
 
