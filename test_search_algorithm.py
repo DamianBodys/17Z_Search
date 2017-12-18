727564ffb34c0fa5f5b1c877ce8df89e43f53004
dbodys94 <dbodys.94@gmail.com>
2017-12-18 10:33:09 +0100

    tests cd

--- test_search_algorithm.py
+++ test_search_algorithm.py
@@ -1,12 +1,21 @@
 """Its very important to install in virtualenv
 pip install WebTest
-pip install PyYaml"""
+also either insert google_appengine, webapp and jinja  libraries in PyCharm library script
+according to this https://www.enkisoftware.com/devlogpost-20141231-1-Python_Google_App_Engine_debugging_with_PyCharm_CE
+or uncomment lines below
+import sys
+sys.path.insert(1, '/root/Downloads/google-cloud-sdk/platform/google_appengine')
+sys.path.insert(1, '/root/Downloads/google-cloud-sdk/platform/google_appengine/lib/webapp2-2.5.2')
+sys.path.insert(1, '/root/Downloads/google-cloud-sdk/platform/google_appengine/lib/jinja2-2.6')
+sys.path.insert(1, '/root/Downloads/google-cloud-sdk/platform/google_appengine/lib/yaml/lib')
+"""
 import unittest
 import webtest
 import search_algorithm
 import json
 from google.appengine.ext import testbed
 from google.appengine.api import search
+
 
 
 class SearchTestCase(unittest.TestCase):
