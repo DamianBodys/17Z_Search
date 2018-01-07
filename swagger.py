57d8bfe31136ef0c7e819144e7bcabe88b31c790
dbodys94 <dbodys.94@gmail.com>
2018-01-07 19:43:15 +0100

    changed host and schemes in swagger.json according to execution environment

--- swagger.py
+++ swagger.py
@@ -1,10 +1,24 @@
 import webapp2
 import json
+import os
+
+
+def get_search_url():
+    if os.environ.get('SERVER_SOFTWARE').startswith('Google App Engine'):
+        return os.environ.get('HTTP_ORIGIN')
+    else:
+        return 'http://localhost:8080'
 
 
 class SwaggerHandler(webapp2.RequestHandler):
     def get(self):
         data = json.load(open('static/swagger.json'))
+        # changing schemes and host in swagger.json to get_search_url()
+        url = get_search_url()
+        data['host'] = url.split('://')[1]
+        del data['schemes'][:]
+        data['schemes'].append(url.split('://')[0])
+        # js = json.dumps(data)
         self.response.headers.add_header("Access-Control-Allow-Origin", "*")
         self.response.headers.add_header('Access-Control-Allow-Methods', 'POST, GET, DELETE, PUT, OPTIONS')
         self.response.headers.add_header('Access-Control-Allow-Headers', 'Content-Type, api_key, Authorization, x-requested-with, Total-Count, Total-Pages, Error-Message')
